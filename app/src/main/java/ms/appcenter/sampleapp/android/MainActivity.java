39c26ab50ed2bf7725f9fadc77c769bb23df46ef
Enrico Vergata <enricodhd@gmail.com>
2020-04-23 10:09:09 +0200

    app secret key

--- app/src/main/java/ms/appcenter/sampleapp/android/MainActivity.java
+++ app/src/main/java/ms/appcenter/sampleapp/android/MainActivity.java
@@ -44,7 +44,7 @@ public class MainActivity extends AppCompatActivity {
                     Analytics.class, Crashes.class, Distribute.class);
         } else {
             // Otherwise use the hardcoded string value here
-            AppCenter.start(getApplication(), "<APP SECRET HERE>",
+            AppCenter.start(getApplication(), "767aff1c-ff32-402f-b79c-145c7a9860ca",
                     Analytics.class, Crashes.class, Distribute.class);
         }
 
